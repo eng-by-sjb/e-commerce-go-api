f0e01af64a1e5fa89a52ab606f63cfe70d853421
Stephen Jersuit Benyah <stephen.benyah.dev@gmail.com>
2024-05-18 18:48:01 +0000

    add types

--- types/types.go
+++ types/types.go
@@ -1,1 +1,25 @@
 package types
+
+import "time"
+
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	CreateUser(user *User) error
+	GetUserByID(id int64) (*User, error)
+}
+
+type RegisterUserPayload struct {
+	FirstName string `json:"first_name" validate:"required"` // json marshal to help convert to json and from json
+	LastName  string `json:"last_name" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=5,max=7"`
+}
+
+type User struct {
+	ID        int64     `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
