0b1ec40332b4caae5e7deff0274fe25d79715b12
Stephen Jersuit Benyah <stephen.benyah.dev@gmail.com>
2024-05-18 18:48:57 +0000

    add hash password util func

--- utils/utils.go
+++ utils/utils.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
 	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var Validate = validator.New()
@@ -24,4 +26,12 @@ func WriteJSON[P any](w http.ResponseWriter, status int, v P) error {
 
 func WriteError(w http.ResponseWriter, status int, err error) {
 	WriteJSON(w, status, map[string]string{"error_msg": err.Error()})
+}
+
+func CreateHashedPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+	return string(hashedPassword), nil
 }
